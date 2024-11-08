5e96b832f88c0e298802da4b4599732cfcb917e2
Ashley Natasha <ashleyotsiula@gmail.com>
2024-11-08 15:17:32 +0300

    react crud

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteQsn, changeAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,23 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleChangeAnswer(event){
+    changeAnswer(id, parseInt(event.target.value))
+  }
+
+  function handleDeleteWhenClicked(){
+    deleteQsn(id)
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={changeAnswer}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteWhenClicked}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const[questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+     .then((res)=> res.json())
+     .then((questions)=> setQuestions(questions))
+  }, [])
+
+  function handleDeleteWhenClicked(id){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE",
+    })
+     .then((res)=> res.json())
+     .then(()=> {
+      const updatedQuestions = questions.filter((question)=> (
+          question.id !== id
+      ))
+      setQuestions(updatedQuestions)
+     })
+  }
+
+  function changeAnswer(id, correctIndex){
+    const configObject= {
+      method: "PATCH",
+      headers:{"Content-Type": "application/json",},
+      body:JSON.stringify({correctIndex}),
+    }
+
+    fetch(`http://localhost:4000/questions/${id}`, configObject)
+     .then((res)=> res.json())
+     .then((updatedQsn)=> {
+      const updatedQuestions = questions.map((question)=> {
+        if(question.id===updatedQsn.id){return updatedQsn}
+        else{return question}
+      })
+      setQuestions(updatedQuestions)
+     })
+  }
+
+  const items= questions.map((x, index) => (
+    <QuestionItem key={index} question={x} 
+    deleteQsn={handleDeleteWhenClicked} 
+    changeAnswer={changeAnswer} />
+  ))
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{items}</ul>
     </section>
   );
 }
